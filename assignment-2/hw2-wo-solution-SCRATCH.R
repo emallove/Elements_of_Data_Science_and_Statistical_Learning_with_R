0b26d742bea4ab6d740858241f955d2e3790c4d1
Ethan Mallove <mallove@fas.harvard.edu>
2019-11-18 18:57:52 -0500

    Add remaining files to git, ...
    which are not already in git

--- assignment-2/hw2-wo-solution-SCRATCH.R
+++ assignment-2/hw2-wo-solution-SCRATCH.R
@@ -14,40 +14,37 @@ wifi_loc_data_matrix2 = wifi_loc_data_matrix2[,-8]
 wifi_loc_data_matrix3 = wifi_loc_data_matrix3[,-8]
 wifi_loc_data_matrix4 = wifi_loc_data_matrix4[,-8]
 
-# plot( x = matrix(), y = matrix(), 
-#       xlab = "Router #",
-#       ylab = "WiFi Signal Strength",
-#       main = "WiFi Signal Strengths when device is located in Room 1")
+X11.options(width=10, height=100)
+par(mfrow = c(2,2))
 
-plot(   xlab = "Router #",
-        ylab = "WiFi Signal Strength",
-        main = "WiFi Signal Strengths when device is located in Room 1",
-        x = rep(c(1,2,3,4,5,6,7), length(wifi_loc_data_matrix1) / 7),
-        y = wifi_loc_data_matrix1[,-8],
-        type = 'n')
+plot_lines <- function(m, l) {
+  plot(   xlab = "Router #",
+          ylab = "WiFi Signal Strength",
+          main = paste("Signal Strength from Room ", l),
+          x = rep(c(1,2,3,4,5,6,7), length(wifi_loc_data_matrix1) / 7),
+          y = wifi_loc_data_matrix1[,-8],
+          type = 'n')
 
-points( x = rep(c(1,2,3,4,5,6,7), length(wifi_loc_data_matrix1) / 7),
-        y = wifi_loc_data_matrix1[,-8],
-        pch = 1)
+  colors <- terrain.colors(nrow(m))
+  i = 1
+  for (r in 1:nrow(m)) {
+      line_ <- c()
+      for (c in 1:ncol(m)) {
+         line_ = append(line_, m[r,c])
+      }
+      lines(1:7, line_, col=colors[i])
+      i = i + 1
+  }
+}
 
-points( x = rep(c(1.25,2.25,3.25,4.25,5.25,6.25,7.25), length(wifi_loc_data_matrix2) / 7),
-        y = wifi_loc_data_matrix2[,-8],
-        pch = 2)
+# points( x = rep(c(1,2,3,4,5,6,7), length(wifi_loc_data_matrix1) / 7),
+#         y = wifi_loc_data_matrix1[,-8],
+#         pch = 1)
 
-points( x = rep(c(1.5,2.5,3.5,4.5,5.5,6.5,7.5), length(wifi_loc_data_matrix3) / 7),
-        y = wifi_loc_data_matrix3[,-8],
-        pch = 3)
+# Create the loop with r and c to iterate over the matrix
 
-points( x = rep(c(1.75,2.75,3.75,4.75,5.75,6.75,7.75), length(wifi_loc_data_matrix4) / 7),
-        y = wifi_loc_data_matrix4[,-8],
-        pch = 4)
-
-# plot(x = rep(c(1,2,3,4,5,6,7), 3),
-#       y = wifi_loc_data_matrix2[,-8],
-#       xlab = "Router #",
-#       ylab = "WiFi Signal Strength",
-#       main = "WiFi Signal Strengths when device is located in Room 2")
-
-# colnames(m) <- c(purrr::map(1:7, purrr::map(1:3, function(x) paste("W", x)))
-colnames(m) <- c("W1", "W2", "W3", "W4", "W5", "W6", "W7", "Room")
+plot_lines(wifi_loc_data_matrix1, "1")
+plot_lines(wifi_loc_data_matrix2, "2")
+plot_lines(wifi_loc_data_matrix3, "3")
+plot_lines(wifi_loc_data_matrix4, "4")
 
